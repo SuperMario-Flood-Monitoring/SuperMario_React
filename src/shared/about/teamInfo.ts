487a377648a47107cb4f9f2ddcf0aeb06293c75d
123dlql2 <123dlql2@naver.com>
2026-07-01 12:46:38 +0900

    about us

--- src/shared/about/teamInfo.ts
+++ src/shared/about/teamInfo.ts
@@ -11,19 +11,19 @@ export const TEAM_MEMBERS: TeamMember[] = [
   {
     roleLabel: '팀장',
     name: '온석태',
-    responsibility: '맡은 역할 (추후 입력)',
-    resumeUrl: '#',
+    responsibility: '역할: React, SWMM 엔진 구현',
+    resumeUrl: 'https://www.rallit.com/hub/resumes/1686371',
   },
   {
     roleLabel: '팀원',
     name: '장윤창',
-    responsibility: '맡은 역할 (추후 입력)',
-    resumeUrl: '#',
+    responsibility: '역할: Django 백엔드 서버 구현',
+    resumeUrl: 'https://www.rallit.com/resumes/1612608@bcobra0/%EC%9E%A5%EC%9C%A4%EC%B0%BD',
   },
   {
     roleLabel: '팀원',
     name: '이현욱',
-    responsibility: '맡은 역할 (추후 입력)',
-    resumeUrl: '#',
-  },
+    responsibility: '역할: LangChain 서비스 서버 구현',
+    resumeUrl: 'https://www.rallit.com/hub/resumes/1735736',
+  }
 ]
